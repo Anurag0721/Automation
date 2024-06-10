628ab4e16424dc682a35a79e6702556c9fdf0d6b
ANURAG <anurag.vijay.chaudhari@gmail.com>
2024-06-10 16:34:07 +0530

    Updated Code

--- src/test/java/tests/test/Demo.java
+++ src/test/java/tests/test/Demo.java
@@ -4,6 +4,7 @@ package tests.test;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,8 +22,8 @@ public class Demo extends BaseTest {
     public static void beforeClass(){
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
-        driver.get("https://www.amazon.in/");
-//        addTestStepPassed("Amazon Site is Launched");
+//        driver.get("https://www.amazon.in/");
+//        add TestStepPassed("Amazon Site is Launched");
         System.out.println("beforeClass");
     }
 
@@ -41,6 +42,8 @@ public class Demo extends BaseTest {
         driver.get("https://www.google.com");
         addTestStepPassed("Google Site is Launched");
         log.info("Google Site is Launched");
+        Assert.assertEquals(driver.getTitle().toLowerCase(),"google");
+        log.info("Title Validated Successfully");
         driver.manage().window().maximize();
         Thread.sleep(3000);
     }
